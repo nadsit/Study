744e8807674f86df28dd482aebe49a4f28995f3d
nadsit <nadsssit@gmail.com>
2021-05-14 19:17:32 +0300

    Added P01.Conditional-Statements-Lab exercises

---
+++ C#-Programming_Basics/02.Conditional_Statements/P01.Conditional-Statements-Lab/06_AreaOfFigures/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _06_AreaOfFigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string figure = Console.ReadLine();
+
+            double num = double.Parse(Console.ReadLine());
+            double num2 = 0.0;
+
+            double result = 0.0;
+
+            if (figure == "square")
+            {
+                result = num * num;
+            }
+            else if (figure == "rectangle")
+            {
+                num2 = double.Parse(Console.ReadLine());
+                result = num * num2;
+            }
+            else if (figure == "circle")
+            {
+                result = Math.PI * (num * num);
+            }
+            else if (figure == "triangle")
+            {
+                num2 = double.Parse(Console.ReadLine());
+                result = (num * num2) / 2;
+            }
+
+            Console.WriteLine($"{result:f3}");
+        }
+    }
+}
---
+++ C#-Programming_Basics/02.Conditional_Statements/P01.Conditional-Statements-Lab/07_ToyShop/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _07_ToyShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double vaccationCost = double.Parse(Console.ReadLine());
+            int puzzles = int.Parse(Console.ReadLine());
+            int dolls = int.Parse(Console.ReadLine());
+            int bears = int.Parse(Console.ReadLine());
+            int toys = int.Parse(Console.ReadLine());
+            int trucks = int.Parse(Console.ReadLine());
+            //пресметни броят на играчките
+            int toysCount = puzzles + dolls + bears + toys + trucks;
+            //пресметни печалбата без отстъпка
+            double profit = puzzles * 2.60 + dolls * 3 + bears * 4.10 + toys * 8.20 + trucks * 2;
+            //провери дали играчките са над 50 и ако да - сметни отстъпка и извади от печалбата
+            if (toysCount >= 50)
+            {
+                profit = profit - 0.25 * profit;
+            }
+            //приспадни наема
+            profit -= profit * 0.1;
+            //провери дали парите за почивка са достатъчни
+            if (profit >= vaccationCost)
+            {
+                Console.WriteLine($"Yes! {(profit - vaccationCost):F2} lv left.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! {(vaccationCost - profit):F2} lv needed.");
+            }
+        }
+    }
+}
